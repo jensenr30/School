934a4054c50e9f7ffc715ffb378ab104d713da1f
jensenr30 <jensenr30@gmail.com>
2013-11-18 22:38:26 -0600

    Started working on Word document for the project. Still need to make a flowchart and discuss the results more. Maybe add some plots of the data? is that necessary?

--- Secant_Colebrook/main.c
+++ Secant_Colebrook/main.c
@@ -1,5 +1,17 @@
-#include <stdio.h> // include the standard input/output header file
+/* main.c
+ * Ryan Jensen
+ * 2013-11-15 (created)
+ * 2013-11-18 (last modified)
+ * 
+ * This program will call the secant_colebrook() function.
+ * This program passes parameters to that function.
+ * The function will calcuate a friction factor from those parameters.
+ * The function will print the results to the user via the terminal.
+*/
 
+
+// include the standard input/output header file
+#include <stdio.h> 
 // include the header file containing the function that solves the colebrook equation for the friction factor f with the secant method.
 #include "secant_colebrook.h"
 
@@ -10,7 +22,7 @@ int main(){
 	float *pointerToReturnValues = NULL;
 	
 	// these are values that will be passed to the secant_colebrook equation.
-	float ReynoldsNumber = 13743;	// unitless
+	float ReynoldsNumber = 8743;	// unitless
 	float diameter = 5;				// millimeters
 	float roughness = 0.0015;		// millimeters
 	
@@ -21,17 +33,16 @@ int main(){
 	float frictionFactor = *pointerToReturnValues;
 	float secantSuccess  = *(pointerToReturnValues+1);
 	
-	
 	// print out information to the user
+	printf("Friction factor = %f\n",frictionFactor);
+	// print success data
 	if(secantSuccess == SECANT_COLEBROOK_SUCCESS){
-		printf("Friction factor = %f\nSuccess = true.\n",frictionFactor);
+		printf("Success = true.\n");
 	}
 	else{
 		printf("Success = false.\n");
 	}
 	
-	
-	
-	
-	return 0;	// end program
+	// end program
+	return 0;
 }
--- Secant_Colebrook/secant_colebrook.h
+++ Secant_Colebrook/secant_colebrook.h
@@ -1,91 +1,88 @@
 /* secant_colebrook.h
  * Ryan Jensen
  * 2013-11-15 (created)
- * 2013-11-15 (last modified)
- * 
+ * 2013-11-18 (last modified)
  * This header file contains the secant_colebrook() function.
- * This function will solve the Colebrook equation for the friction factor f numerically.
+ * This function will solve the Colebrook equation for the friction factor f
+ * numerically.
 */
 
-//used for the nice math functions like log10()
+//used for the nice math functions like log10() and sqrtf()
 #include <math.h>
 
+// this is a flag that denotes success in finding a zero
+#define SECANT_COLEBROOK_SUCCESS 1.0f
+// this is a flag that denotes a failure to find a zero
+#define SECANT_COLEBROOK_FAILURE 0.0f
 
-// don't debug the function unless instructed to.
-#ifndef SECANT_DEBUG
-#define SECANT_DEBUG 0
-#endif
-
-
-// this function numberically solves the Colebrook Equation for the friction factor f.
-// the return value is a pointer to two floating point values.
-// the first floating point number is the friction factor f
-// the second floating point number is an error flag.
-#define SECANT_COLEBROOK_SUCCESS 1.0f	// this is a flag that denotes success in finding a zero
-#define SECANT_COLEBROOK_FAILURE 0.0f	// this is a flag that denotes a failure to find a zero
-float * secant_colebrook(float ReynoldsNumber, float diameter, float roughness){
+/// this function solves the Colebrook equation for the friction factor, f.
+/// the return value is a pointer to two floating point values.
+/// the first floating point number is the friction factor f
+/// the second floating point number is an error flag.
+float * secant_colebrook(float Re, float D, float epsilon){
 	
 	///-------------------------------------------------------------------------
 	///		SET UP VARIABLES
 	///-------------------------------------------------------------------------
+	
 	// this is where we store the frictionFactor.
 	// ffact[0] is the first guess.
 	// ffact[1] is the second guess.
 	// ffact[2] is the next guess.
 	static float ffact[3];
-	
 	// this stores the difference between one side and the other side of the
 	// colebrook equation. Lets call it y.
 	// y[0] is for the first guess.
 	// y[1] is for the second guess.
 	// y[2] is for the next guess.
 	float y[3];
-	
 	// this stores the slope of the line between.
 	float slope;
 	
-	// this is the tolerance for relative error in our value.
-	float tolerance = 0.01;
-	//this stores the relative error at the current itteration
-	float relativeError;
-	
-	// this keeps track of how many times the program has gone through the while() loop.
-	int itterations;
+	// this is the f_tol for relative error in our value.
+	float f_tol = 0.01;
+	//this stores the relative error at the current iteration
+	float relErr;
+	// this keeps track of how many times the function has gone through the loop.
+	int iter;
 	//this is the maximum number of times that you can go through the while() loop.
-	int maxItterations = 200;
+	int maxItt = 200;
 	
 	// define an array of floating point values with two elements.
 	// the first element will store the friction factor.
 	// the second value will store the success value.
 	static float returnValues[2];
 	
-	
 	///-------------------------------------------------------------------------
 	///		EVALUATE THE ROOT OF THE COLEBROOK EQUATION
 	///-------------------------------------------------------------------------
 	
 	//initial guesses for the friction factor
-	ffact[0] = 0.8;
-	ffact[1] = 0.4;
+	ffact[0] = 0.1;
+	ffact[1] = 0.01;
 	//initial value to make sure we can enter the loop
 	ffact[2] = 10;
-	// start itterations at 0
-	itterations = 0;
-	// make sure the relative error is greater than tolerance to start
-	relativeError = 100*tolerance;
+	// start iter at 0
+	iter = 0;
+	// make sure the relative error is greater than f_tol to enter the loop
+	relErr = 100*f_tol;
 	
-	while(relativeError > tolerance && itterations < maxItterations || ffact[2] >= 0.0){
+	while(relErr > f_tol && iter < maxItt && ffact[2] >= 0.0){
 		
+		// evaluate the Colebrook equation at point 0
+		y[0]= (1/sqrtf(ffact[0]));
+		y[0]+= 2*log10((epsilon/(3.7f * D))+(2.51f/(Re*sqrtf(ffact[0]))));
+		// evaluate the Colebrook equation at point 1
+		y[1]= (1/sqrtf(ffact[1]));
+		y[1]+= 2*log10((epsilon/(3.7f * D))+(2.51f/(Re*sqrtf(ffact[1]))));
 		
-		y[0] = (1/sqrtf(ffact[0]))  +  2*log10( (roughness/(3.7f * diameter)) + (2.51f/(ReynoldsNumber*sqrtf(ffact[0]))) );
-		y[1] = (1/sqrtf(ffact[1]))  +  2*log10( (roughness/(3.7f * diameter)) + (2.51f/(ReynoldsNumber*sqrtf(ffact[1]))) );
-		
-		// if the slope is going to be 0 or infinite, break the loop and return a failure condition.
+		// if the slope is 0 or infinite, break and return a failure flag.
 		if(ffact[1] == ffact[0] || y[1] == y[0])break;
 		
 		// calculate the slope
 		slope = (y[1]-y[0])/(ffact[1]-ffact[0]);
 		
+		// calculate where the next x value will be located
 		ffact[2] = ffact[0] - y[0]/slope;
 		
 		
@@ -93,46 +90,43 @@ float * secant_colebrook(float ReynoldsNumber, float diameter, float roughness){
 		ffact[0] = ffact[1];
 		// store new ffact value in the second's guess spot
 		ffact[1] = ffact[2];
-		//increment the itteration counter.
-		itterations++;
+		//increment the iteration counter.
+		iter++;
 		
 		//calculate relative error
-		relativeError = (y[1]-y[0])/y[1];
+		relErr = (ffact[1]-ffact[0])/ffact[1];
 		// take absolute value of relative error calculation
-		if(relativeError < 0) relativeError *= -1.0f;
+		if(relErr < 0) relErr *= -1.0f;
 		
 	}
+	
+	///-------------------------------------------------------------------------
+	///		DETERMINE RETURN VALUES AND EXIT FUNCTION
+	///-------------------------------------------------------------------------
+	
 	// the program has failed if any of the following are true:
-	// 		- the relative error is still larger than the tolerance
-	//		- the loop went through too many itterations
+	// 		- the relative error is still larger than the f_tol
+	//		- the loop went through too many iter
 	//		- the slope of the line if infinite or zero
-	if(relativeError > tolerance || itterations >= maxItterations || y[0] == y[1] || ffact[2] < 0){
-		// set succes flag to FAILURE
-		returnValues[0] = 0.0f;
+	//		- the ffact value that will be returned is negative
+	if(relErr>f_tol || iter>=maxItt || y[0]==y[1] || ffact[2]<0){
+		// return the value of the invalid friction factor (whatever it may be)
+		returnValues[0] = ffact[2];
+		// set success flag to FAILURE
 		returnValues[1] = SECANT_COLEBROOK_FAILURE;
 		return returnValues;
 	}
 	// you must have succeeded if nothing went wrong.
 	else{
-		// set success flag to SUCCESS
+		// return the value of the correctly calculated friction factor
 		returnValues[0] = ffact[2];
+		// set success flag to SUCCESS
 		returnValues[1] = SECANT_COLEBROOK_SUCCESS;
 		return returnValues;
 	}
-	
-	
-	
-	
-	
-	
-	
 	// the function should never get to this point.
 	// but if it does, return a failure flag.
 	returnValues[0] = 0.0f;
 	returnValues[1] = SECANT_COLEBROOK_FAILURE;
 	return returnValues;
 }
-
-
-
-
